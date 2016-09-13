2701313cfcdb3d531ae5968389e187e856b22dea
shimron <shimron.shen@chiefclouds.com>
2016-09-13 14:37:33 +0800

    add invoke codes

--- ex01/ex01.go
+++ ex01/ex01.go
@@ -30,7 +30,23 @@ func (cc *SimpleChainCode) Init(stub shim.ChaincodeStubInterface, function strin
 }
 
 func (cc *SimpleChainCode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	return nil, nil
+	if function != "invoke" {
+		return nil, errors.New("invalid function name. Expecting invoke")
+	}
+	if len(args) != 2 {
+		return nil, errors.New("incorrect number of arguments. Expecting 2")
+	}
+	var A string
+	var Aval int
+	var err error
+	A = args[0]
+	Aval, err = strconv.Atoi(args[1])
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("Aval=%d\n", Aval)
+	err = stub.PutState(A, []byte(args[1]))
+	return nil, err
 }
 
 func (cc *SimpleChainCode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
